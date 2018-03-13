a1b1a9b98f4bc4c8c7064d68abd077a2e55ca066
Sara <sara@gmail.com>
2018-03-13 11:15:32 +0100

    main puesto

--- Train/Debug/src/subdir.mk
+++ Train/Debug/src/subdir.mk
@@ -4,13 +4,22 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/Train.cpp 
+../src/Line.cpp \
+../src/Stop.cpp \
+../src/Train.cpp \
+../src/main.cpp 
 
 OBJS += \
-./src/Train.o 
+./src/Line.o \
+./src/Stop.o \
+./src/Train.o \
+./src/main.o 
 
 CPP_DEPS += \
-./src/Train.d 
+./src/Line.d \
+./src/Stop.d \
+./src/Train.d \
+./src/main.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
