1417c937a46dae5d0070dbf2f65b1b1a4bf10723
Fer <fer.blazquezl@gmail.com>
2018-03-14 08:45:13 +0100

    new tasks

--- Train/src/main.cpp
+++ Train/src/main.cpp
@@ -7,31 +7,20 @@
 //============================================================================
 #include "Train.h"
 
-void task2(){
-	Stop s1("Asgard", 1);
-	Line l;
-	l.addStop(s1);
-	Stop s2("Midgard", 4);
-	l.addStop(s2);
-	Stop s3("Hel", 2);
-	l.addStop(s3);
-	Train t1(1,l);
-	t1.goToEnd();
+void task2(Train t){
+	t.goToEnd();
 }
 
-void task1(void){
-	Stop s1("Paraíso", 3);
-	Line l;
-	l.addStop(s1);
-	Stop s2("Limbo", 4);
-	l.addStop(s2);
-	Stop s3("Infierno", 2);
-	l.addStop(s3);
-	Train t1(1,l);
-	t1.goToEnd();
+void task1(Train t){
+	t.go();
+}
+
+void newTrain (int speed, Line l){
+	Train(speed, l);
 }
 
 int main(){
+
 	thread t(task1);
 	thread t2(task2);
 	t.join();
