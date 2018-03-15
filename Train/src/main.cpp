97636d7a648963f2410863f9aef4009d7f4fa26d
Sara <sara@gmail.com>
2018-03-15 09:33:47 +0100

    added new lines (main)

--- Train/src/main.cpp
+++ Train/src/main.cpp
@@ -15,52 +15,30 @@ void task1(Train t){
 	t.go();
 }
 
-
+void stopCreation(Line& l, string nameStop, int distance){
+	Stop s(nameStop, distance);
+	l.addStop(s);
+}
 
 int main(){
-	Stop sl1("Asgard", 1);
-	Stop sl2("Midgard", 4);
-	Stop sl3("Hel", 2);
-	Stop s1("Paraíso", 3);
-	Stop s2("Limbo", 4);
-	Stop s3("Infierno", 2);
 	Line l1;
+	stopCreation(l1,"Asgard", 1);
+	stopCreation(l1,"Midgard", 4);
+	stopCreation(l1,"Hel", 2);
 	Line l2;
-	l1.addStop(sl1);
-	l1.addStop(sl2);
-	l1.addStop(sl3);
-	l2.addStop(s1);
-	l2.addStop(s2);
-	l2.addStop(s3);
 	Train train1 (1,l1);
+	stopCreation(l2, "Paraíso", 3);
+	stopCreation(l2, "Limbo", 4);
+	stopCreation(l2, "Midgard", 2);
+	stopCreation(l2, "Infierno", 2);
 	Train train2 (1,l2);
+	stopCreation(l2, "Caribe", 1);
+	stopCreation(l2, "Paraíso", 3);
+	stopCreation(l2, "Antártida", 3);
+	stopCreation(l2, "Hel", 2);
 	thread t1(task2, train1);
 	thread t2(task2, train2);
 	t1.join();
-	//train1.stop();
 	t2.join();
-
 }
- void task2(){
- 	Stop s1("Asgard", 1);
- 	Line l;
- 	l.addStop(s1);
- 	Stop s2("Midgard", 4);
- 	l.addStop(s2);
- 	Stop s3("Hel", 2);
- 	l.addStop(s3);
- 	Train t1(1,l);
- 	t1.goToEnd();
- }
 
- void task1(void){
- 	Stop s1("Paraíso", 3);
- 	Line l;
- 	l.addStop(s1);
- 	Stop s2("Limbo", 4);
- 	l.addStop(s2);
- 	Stop s3("Infierno", 2);
- 	l.addStop(s3);
- 	Train t1(1,l);
- 	t1.goToEnd();
- }
